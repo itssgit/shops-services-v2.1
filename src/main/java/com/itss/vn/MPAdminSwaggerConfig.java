2d17a0f641ef06be8361a199d236d675d3783071
MINH THANG <MINH THANG@MINHTHANG-PC>
2017-09-13 19:15:10 +0700

    add header authorization

--- src/main/java/com/itss/vn/MPAdminSwaggerConfig.java
+++ src/main/java/com/itss/vn/MPAdminSwaggerConfig.java
@@ -1,12 +1,24 @@
 package com.itss.vn;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+
+import com.itss.vn.authorization.model.AuthorizationRequest;
+import com.itss.vn.authorization.service.AuthorizationService;
+import com.itss.vn.shops.entity.Account;
+
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,9 +29,32 @@ public class MPAdminSwaggerConfig {
 
     @Value("${swagger.host.url}")
     private String swaggerHostUrl;
+    
+    @Value("${oauth2.admin.id}")
+	private String clientId;
+	
+	@Autowired
+	private AuthorizationService authorizationService;
 
     @Bean
     public Docket petApi() {
+    	
+    	AuthorizationRequest authorizationRequest = new AuthorizationRequest();
+		authorizationRequest.setUserName("thang.vm");
+		authorizationRequest.setPassWord("1234");
+		
+		Account account = authorizationService.authorizeAccount(authorizationRequest);
+		OAuth2AccessToken token = authorizationService.generateAccessToken(account, clientId);
+		
+		/*Parameter parameter = new Parameter("Authorization", null, "Bear " + token.getValue(), true, true, null, null, null, "String", "String");
+		List<Parameter> parameters = new ArrayList<>();
+		parameters.add(parameter);*/
+		
+		Parameter parameter = new ParameterBuilder().name("authorization").defaultValue("Bear " + token.getValue())
+								.description("header").modelRef(null).parameterType("String").required(false).build();
+		List<Parameter> parameters = new ArrayList<>();
+		parameters.add(parameter);
+		
         return new Docket(DocumentationType.SWAGGER_2)
                 .host(swaggerHostUrl)
                 .select()
@@ -27,7 +62,8 @@ public class MPAdminSwaggerConfig {
                 .paths(PathSelectors.any())
                 .build()
                 .pathMapping("/")
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .globalOperationParameters(parameters);
     }
 
     private ApiInfo apiInfo() {
